1327bcaa502416e32608f64ff01f8f2d43be552f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-24 22:37:38 +0000

    Deploy CharlesStover/charlesstover to github.com/CharlesStover/charlesstover.git:gh-pages

---
+++ precache-manifest.9ca87e92c5ba843db20d9b43f1e9f77a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "10bc547bf8954c85f3a6",
+    "url": "/static/js/main.10bc547b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9532a5c3e9c390881064",
+    "url": "/static/js/2.9532a5c3.chunk.js"
+  },
+  {
+    "revision": "f12b1b8f7bdfdb01e35538f67a4ed84f",
+    "url": "/static/media/jin-haonan.f12b1b8f.png"
+  },
+  {
+    "revision": "f87973374ea3efa0ffe072363e249e54",
+    "url": "/static/media/ahmad-elsokkary.f8797337.jpg"
+  },
+  {
+    "revision": "030c2442dd0869c1f74c8ca1a12ef061",
+    "url": "/static/media/blake-moore.030c2442.jpg"
+  },
+  {
+    "revision": "c73b24bfacebb25da5f2fef7c4f983aa",
+    "url": "/static/media/jacob-sowles.c73b24bf.jpg"
+  },
+  {
+    "revision": "dcf7c470adbf7ec7e093a0f1993550c1",
+    "url": "/static/media/josh-sherman.dcf7c470.jpg"
+  },
+  {
+    "revision": "c5fde77b58eb467e5652885b876f6a8c",
+    "url": "/static/media/ryan-clausen.c5fde77b.jpg"
+  },
+  {
+    "revision": "ea3ac63f84a7478d6c6b3cdfc06d0594",
+    "url": "/static/media/spritesheet2gif.ea3ac63f.png"
+  },
+  {
+    "revision": "ffb19f6d1c3355153169dc1944dd9421",
+    "url": "/static/media/rpg-overworld-engine.ffb19f6d.gif"
+  },
+  {
+    "revision": "7669cf7b172ef194b3d6fcd74400d6f7",
+    "url": "/static/media/expert.7669cf7b.png"
+  },
+  {
+    "revision": "bc93b03d8299f8f5939d67e53f98974d",
+    "url": "/static/media/proficient.bc93b03d.png"
+  },
+  {
+    "revision": "f0edb06f163586c33ac5499c196b94c9",
+    "url": "/static/media/github.f0edb06f.png"
+  },
+  {
+    "revision": "296e9e0d3bbaef050dc5fb522c72c6d9",
+    "url": "/index.html"
+  }
+];
